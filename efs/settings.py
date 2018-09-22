9da8ac4bbf21f2e56a37a8214c3d5eb16532ae2d
ruhidamda <35614558+ruhidamda@users.noreply.github.com>
2018-09-22 12:03:35 -0500

    Update settings.py

--- efs/settings.py
+++ efs/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'zm4m@hi^xwe1ca!x$urt%f8br+y2pu-o7hy(ze_&o(-eryb5x0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
